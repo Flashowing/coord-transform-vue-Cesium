24e58dd0cbc0633edcb739610804d72baf9e4bb3
flashowing <flashowingy@gmail.com>
2021-04-02 23:46:01 +0800

    使用vue+ElementUI构建Cesium坐标转换工具

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-main>
+        <DegreesToCartesian></DegreesToCartesian>
+      </el-main>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import DegreesToCartesian from "@/components/DegreesToCartesian";
+export default {
+  name: 'App',
+  components: {
+    DegreesToCartesian
+  },
+  data:function (){
+    return {
+      msg: 'Hello'
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
