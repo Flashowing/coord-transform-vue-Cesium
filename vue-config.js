dd5f269197d3ad66c3536acf3b06ae55e8e2d35a
flashowing <flashowingy@gmail.com>
2021-04-23 23:54:40 +0800

    地图修改

--- vue-config.js
+++ vue-config.js
@@ -1,4 +1,6 @@
 const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
     configureWebpack: {
         //支持jquery
@@ -7,6 +9,13 @@ module.exports = {
                 $:"jquery",
                 jQuery:"jquery",
                 "windows.jQuery":"jquery"
+            }),
+            new CompressionPlugin({
+                algorithm: 'gzip',
+                test: /\.(js|css|less)$/, // 匹配文件名
+                threshold: 10240, // 对超过10k的数据压缩
+                minRatio: 0.8,
+                deleteOriginalAssets: true // 删除源文件
             })
         ]
     },
